d041b55a1f2752ad35b4322a275237577ae02bef
Rafael <rafaelfr@outlook.com>
2020-10-05 09:41:56 -0300

    feat: accept single url as prop
    This simplifies the api and removes the need to build an object to load a font.
    As google offers a url already with query params, this hook will just load the font as is.
    
    BREAKING CHANGE: removed fonts prop

--- src/index.js
+++ src/index.js
@@ -1,40 +1,24 @@
 import { useEffect } from 'react'
-import { arrayOf, number, oneOfType, shape, string } from 'prop-types'
+import { string } from 'prop-types'
 
-const createElement = fonts => {
-  const families = fonts
-    .reduce((accumulator, font) => {
-      const family = font.name.replace(' ', '+')
-      const weight = (font.weight && `:${font.weight.join(',')}`) || ''
-      return [...accumulator, `${family}${weight}`]
-    }, [])
-    .join('|')
-
-  const link = document.createElement('link')
-  link.rel = 'stylesheet'
-  link.href = `https://fonts.googleapis.com/css?family=${families}`
-
-  return link
+const createElement = url => {
+  const element = document.createElement('link')
+  element.rel = 'stylesheet'
+  element.href = url
+  return element
 }
 
-const ReactFontLoader = ({ fonts }) => {
+const ReactFontLoader = ({ url }) => {
   useEffect(() => {
-    const element = createElement(fonts)
+    const element = createElement(url)
     document.head.appendChild(element)
-
     return () => document.head.removeChild(element)
   }, [])
-
   return null
 }
 
 ReactFontLoader.propTypes = {
-  fonts: arrayOf(
-    shape({
-      name: string.isRequired,
-      weights: arrayOf(oneOfType([string, number])),
-    }),
-  ).isRequired,
+  url: string.isRequired,
 }
 
 export default ReactFontLoader
